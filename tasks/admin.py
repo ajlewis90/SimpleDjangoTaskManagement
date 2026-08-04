8792a786c0f1046a38e2c850cb912701df16a751
ajlewis90 <arthur.lewis420@gmail.com>
2026-08-04 14:50:12 +1200

    Registered Task model class with the django admin interface

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Task #"." just means start from the same parent directory of this file
+#from tasks.models import Task #Even this works
+
 # Register your models here.
+registered_models = [Task] #can add more models to this list
+
+admin.site.register(registered_models)
+
