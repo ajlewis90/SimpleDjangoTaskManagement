c89903808e4849b34438ce9e97396d217bd29974
ajlewis90 <arthur.lewis420@gmail.com>
2026-08-10 15:16:00 +1200

    Added deployment settings for secret key, allowed hosts, debug and database

--- SimpleDjangoTaskManagement/settings.py
+++ SimpleDjangoTaskManagement/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vfo^619h(48dhx4)%jn$*c@g9((%fq(o^60h334^yif9kbesoh'
+# SECRET_KEY = 'django-insecure-vfo^619h(48dhx4)%jn$*c@g9((%fq(o^60h334^yif9kbesoh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if 'PYTHONPATH' in os.environ:
+    DEBUG = False
 
-ALLOWED_HOSTS = []
+    SECRET_KEY = os.environ['SECRET_KEY']
+
+    ALLOWED_HOSTS = ['.ap-southeast-2.elasticbeanstalk.com']
+else:
+    DEBUG = True
+
+    SECRET_KEY = 'django-insecure-vfo^619h(48dhx4)%jn$*c@g9((%fq(o^60h334^yif9kbesoh'
+
+    ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -76,16 +86,28 @@ WSGI_APPLICATION = 'SimpleDjangoTaskManagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'simpledjangotasks_db',
-        'USER': 'postgres',
-        'PASSWORD': 'Cellfone01', #Put in the password to your local pgadmin
-        'HOST': 'localhost',
-        'PORT': '5432',
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+            'default': {
+                'ENGINE':'django.db.backends.postgresql_psycopg2',
+                'NAME': os.environ['RDS_DB_NAME'],
+                'USER': os.environ['RDS_USERNAME'],
+                'PASSWORD': os.environ['RDS_PASSWORD'],
+                'HOST': os.environ['RDS_HOSTNAME'],
+                'PORT': os.environ['RDS_PORT'],
+            }
+        }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'simpledjangotasks_db',
+            'USER': 'postgres',
+            'PASSWORD': 'Cellfone01', #Put in the password to your local pgadmin
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
     }
-}
 
 
 # Password validation
